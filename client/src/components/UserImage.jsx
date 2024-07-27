5f97574434eeda6447c336ba4c125acb48f85fa5
thanusree45 <143024576+thanusree45@users.noreply.github.com>
2024-07-27 19:45:25 +0530

    Update UserImage.jsx

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -9,7 +9,7 @@ const UserImage = ({image, size="60px"})=>{
             width={size}
             height={size}
             alt="user"
-            src={`http://localhost:3001/assets/${image}`}
+            src={`https://peernet-backend.onrender.com/assets/${image}`}
             />
         </Box>
     )
